ef7388a7c5ecd99044339d8215f38188f2794605
JimcostDev <rjak727@gmail.com>
2021-02-23 19:11:13 -0500

    Creating records in Angular through Component, Services and Class

--- university/src/app/app-routing.module.ts
+++ university/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CourseListComponent } from './component/course-list/course-list.component';
+import { CourseSaveComponent } from './component/course-save/course-save.component';
 
 const routes: Routes = [
-  { path:'course-list', component: CourseListComponent }
+  { path:'course-list', component: CourseListComponent },
+  { path:'course-save', component: CourseSaveComponent }
 ];
 
 @NgModule({
--- university/src/app/service/course.service.ts
+++ university/src/app/service/course.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Course } from '../domain/course';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,15 @@ export class CourseService {
   public url: string;
 
   constructor(public httpClient: HttpClient) {
-    this.url = './assets/MOCK_DATA.json';
-    //this.url = 'http://localhost/University.API/api/Courses/'
+    //this.url = './assets/MOCK_DATA.json';
+    this.url = 'http://localhost/University.API/api/Courses/'
     // this.url = environment.apiUrl + 'api/Courses/';
   }
   public getAll(): Observable<any> {
     return this.httpClient.get(this.url);
+  }
+
+  public save(course: Course): Observable<any> {
+    return this.httpClient.post(this.url, course);
   }
 }
